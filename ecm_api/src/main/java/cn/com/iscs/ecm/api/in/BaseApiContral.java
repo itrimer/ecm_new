9800de9727057247c1adc4de6562c6df72915f1f
xuchun <xuchun1121121@163.com>
2016-02-22 16:55:06 +0800

    删除日志

--- ecm_api/src/main/java/cn/com/iscs/ecm/api/in/BaseApiContral.java
+++ ecm_api/src/main/java/cn/com/iscs/ecm/api/in/BaseApiContral.java
@@ -4,7 +4,8 @@ package cn.com.iscs.ecm.api.in;
  * Created by xuchun on 16/2/22.
  */
 public class BaseApiContral {
-
-
+    //获取来源，判断是属于哪个平台，ERP，api过来的。
+    //判断notify_id，是否是发送过的请求。如果是，就直接反馈true
+    //
 
 }
